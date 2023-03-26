b21694218a51b11e00d4799618bb8024dfd44bc2
Allison Huang <111381124+AllisonH12@users.noreply.github.com>
2023-03-26 03:10:10 +0000

    add title and desc

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -64,6 +64,8 @@ export default function MyApp({ Component, emotionCache = clientSideEmotionCache
   return (
     <CacheProvider value={emotionCache}>
       <Head>
+      <title>Capy Chat: the smartest chat for kids with autism</title>
+      <meta name="description" content="Smart Chat with Animals like Dog Chat, Capybara Chat, Cat Chat. Help kids to communicate and learn" />
         <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
       <CssVarsProvider defaultMode='light' theme={theme}>
